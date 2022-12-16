c1edc9344b1735f022d7587c33c9378d63f34c6b
Ravikiran Reddy Balemla <56499420+kirannani168@users.noreply.github.com>
2022-12-16 09:21:17 +0530

    Form Updated

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
       <HookCounterFour/>
       <hr/>
       <FormTest/>
-      <hr/>
     </div>
   );
 }
--- src/Components/FormTest.js
+++ src/Components/FormTest.js
@@ -1,35 +1,58 @@
 import React, { useState } from 'react'
 
 function FormTest() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const nameHandler = (e)=>{
-        setName(e.target.value)
-    }
-    const emailHandler = (e) =>{
-            setEmail(e.target.value)
-    }
-    const submitHandler = (e) =>{
-        alert('You entered: '+ name + email);
-        e.preventDefault()
-    }
-  return (
+   const [details, setDetails] = useState({name:'', email:''});
+   const setName = (e) =>{
+    setDetails({...details, name:e.target.value})
+   }
+   const setEmail = (e) =>{
+    setDetails({...details, email:e.target.value})
+   }
+
+   return(
     <div>
-        <h3>Form Test</h3>
-        <span>
-            {name}<br/>
-            {email}
-        </span>
-        <form onSubmit={(e)=>{submitHandler(e)}}>
-            <label>Name: </label>
-            <input type='text' value={name} onChange={(e)=>{nameHandler(e)}}/>
-            <br></br>
-            <label>Email</label>
-            <input type='email' value={email} onChange={(e)=>{emailHandler(e)}}/>
-            <button type='submit' value='Submit'>Submit</button>
-        </form>
+        <h3>{details.name} and {details.email}</h3>
+        <input type='text' onChange={e=>{setName(e)}}/>
+        <input type="text" onChange={(e)=>{setEmail(e)}}/>
     </div>
-  )
+   )
 }
 
 export default FormTest
+
+
+// import React, { useState } from 'react'
+
+// function FormTest() {
+//     const [name, setName] = useState('')
+//     const [email, setEmail] = useState('')
+//     const nameHandler = (e)=>{
+//         setName(e.target.value)
+//     }
+//     const emailHandler = (e) =>{
+//             setEmail(e.target.value)
+//     }
+//     const submitHandler = (e) =>{
+//         alert('You entered: '+ name + email);
+//         e.preventDefault()
+//     }
+//   return (
+//     <div>
+//         <h3>Form Test</h3>
+//         <span>
+//             {name}<br/>
+//             {email}
+//         </span>
+//         <form onSubmit={(e)=>{submitHandler(e)}}>
+//             <label>Name: </label>
+//             <input type='text' value={name} onChange={(e)=>{nameHandler(e)}}/>
+//             <br></br>
+//             <label>Email</label>
+//             <input type='email' value={email} onChange={(e)=>{emailHandler(e)}}/>
+//             <button type='submit' value='Submit'>Submit</button>
+//         </form>
+//     </div>
+//   )
+// }
+
+// export default FormTest
