932010a9ecc1daff8d2fd15972b6bcd4fb6241d6
Ravikiran Reddy Balemla <56499420+kirannani168@users.noreply.github.com>
2022-12-16 10:28:24 +0530

    Updated useEffect with conditions
    Dependencies added as second parameter for useEffect Hook

--- src/Components/UseEffect.js
+++ src/Components/UseEffect.js
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from 'react'
 
 function UseEffect() {
     const [count, setCount] = useState(0);
-    const countHandler = () =>{
-        setCount(count+1)
+    const [name, setName] = useState('');
+    const countHandler = () => {
+        setCount(count + 1)
     }
-    useEffect(()=>{
-        document.title =   `You clicked ${count} times`
-    })
+    const nameHandler = (e) => {
+        setName(e.target.value)
+    }
+    useEffect(() => {
+        console.log("Rendered")
+        document.title = `You clicked ${count} times`
+    }, [count])
     return (
         <div>
             <h3>useEffect</h3>
             <b>
                 <pre>
-                <p>When the count get updated, document title also get updated</p>
-                    {count}
+                    <p>When the count get updated, document title also get updated</p>
+                    {count} <br />
+                    {name}
                 </pre>
             </b>
+            <input type='text' value={name} onChange={(e) => nameHandler(e)} />
             <button onClick={countHandler}>Click Here</button>
         </div>
     )
